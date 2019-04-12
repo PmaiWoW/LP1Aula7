f8521ce40fc93832673d0620c90f2e9be55a6e3b
PmaiWoW <PmaiWoW@Gmail.com>
2019-04-12 12:15:36 +0100

    On BookManager, add code to ask user the number of books and their titles and author names, displaying both titles and authors for all books given, after the fact

--- BookManager/Program.cs
+++ BookManager/Program.cs
@@ -6,7 +6,51 @@ namespace BookManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declarar variáveis
+            int bookNum;
+            string title, author;
+            Book[] books;
+
+            // Pedir ao utilizador o nº de livros que quer
+            Console.WriteLine("How many books do you want?");
+            bookNum = Convert.ToInt32(Console.ReadLine());
+
+            // Inicializar o array books
+            books = new Book[bookNum];
+
+            // Cicle for que pergunta para cada livro o seu título e autor e
+            // os mostra ao utilizador
+            for(int i = 0; i < bookNum; i++)
+            {
+                Console.WriteLine("\nInsert the book's title:");
+                title = Console.ReadLine();
+                Console.WriteLine("\nInsert the book's author:");
+                author = Console.ReadLine();
+                // Criar o objeto book com o título e autor dados
+                Book book = new Book(title, author);
+                // Guardar o objeto book criado no array de objetos book 
+                // (books) com o título e autor dados
+                books[i] = book;
+            }
+
+            // Para cada objeto book no array books, mostrar o título e autor
+            for(int i = 0; i < books.Length; i++)
+            {
+
+                Console.WriteLine($"\nTitle: {books[i].GetTitle()}" +
+                    $"(Title Length: {books[i].GetTitle().Length})");
+                Console.WriteLine($"Author: {books[i].GetAuthor()}\n");
+            }
+            
+            /* Exemplo do pdf de aula
+            Book book = new Book("Harry ", "J.K Rowling");
+
+            // Change name of book after creating it
+            book.SetTitle("Harry Potter and the Chamber of Secrets");
+
+            // Damn, what was the title?
+            Console.WriteLine(book.GetTitle());
+            */
         }
     }
 }
