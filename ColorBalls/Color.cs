f958ed161fedf4d4f515d76bb42e2fbab27ff3f6
Pedro Inácio <PmaiWoW@Gmail.com>
2019-04-22 13:56:28 +0100

    Simplify the Color class methods, saving lines of code, making the methods more comprehensible

--- ColorBalls/Color.cs
+++ ColorBalls/Color.cs
@@ -10,7 +10,6 @@ namespace ColorBalls
         private byte gVal;
         private byte bVal;
         private byte alphaVal;
-        private byte gscaleVal;
 
         public Colour(byte rVal, byte gVal, byte bVal)
         {
@@ -28,52 +27,23 @@ namespace ColorBalls
             this.alphaVal = alphaVal;
         }
 
-        public byte GetRedValue()
-        {
-            return this.rVal;
-        }
+        public byte GetRedValue() => this.rVal;
 
-        public byte GetGreenValue()
-        {
-            return this.gVal;
-        }
+        public byte GetGreenValue() => this.gVal;
 
-        public byte GetBlueValue()
-        {
-            return this.bVal;
-        }
+        public byte GetBlueValue() => this.bVal;
 
-        public byte GetAlphaValue()
-        {
-            return this.alphaVal;
-        }
+        public byte GetAlphaValue() => this.alphaVal;
 
-        public byte GetGrayscaleValue()
-        {
-            this.gscaleVal = Convert.ToByte((this.rVal + this.gVal + 
-                this.bVal) / 3);
-            return this.gscaleVal;
-        }
+        public byte GetGrayscaleValue() => (byte) ((rVal + gVal + bVal) / 3);
 
-        public void SetRedValue(byte rVal)
-        {
-            this.rVal = rVal;
-        }
+        public void SetRedValue(byte rVal) => this.rVal = rVal;
 
-        public void SetGreenValue(byte gVal)
-        {
-            this.gVal = gVal;
-        }
+        public void SetGreenValue(byte gVal) => this.gVal = gVal;
 
-        public void SetBlueValue(byte bVal)
-        {
-            this.bVal = bVal;
-        }
+        public void SetBlueValue(byte bVal) => this.bVal = bVal;
 
-        public void SetAlphaValue(byte alphaVal)
-        {
-            this.alphaVal = alphaVal;
-        }
+        public void SetAlphaValue(byte alphaVal) => this.alphaVal = alphaVal;
 
 
     }
